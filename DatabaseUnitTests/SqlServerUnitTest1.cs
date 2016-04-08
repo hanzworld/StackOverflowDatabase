2edf61dec894a579174110ec177107a2945ebb8e
Hannah <hannah@test.xero.com>
2016-04-09 04:00:07 +1200

    Add SSDT Unit Tests to find datetime(2) > 7

---
+++ DatabaseUnitTests/SqlServerUnitTest1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+using Microsoft.Data.Tools.Schema.Sql.UnitTesting;
+using Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DatabaseUnitTests
+{
+    [TestClass()]
+    public class SqlServerUnitTest1 : SqlDatabaseTestClass
+    {
+
+        public SqlServerUnitTest1()
+        {
+            InitializeComponent();
+        }
+
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            base.InitializeTest();
+        }
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            base.CleanupTest();
+        }
+
+        #region Designer support code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction dbo_Posts_GetByUserTest_TestAction;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SqlServerUnitTest1));
+            Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.EmptyResultSetCondition emptyResultSetCondition1;
+            this.dbo_Posts_GetByUserTestData = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestActions();
+            dbo_Posts_GetByUserTest_TestAction = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.SqlDatabaseTestAction();
+            emptyResultSetCondition1 = new Microsoft.Data.Tools.Schema.Sql.UnitTesting.Conditions.EmptyResultSetCondition();
+            // 
+            // dbo_Posts_GetByUserTest_TestAction
+            // 
+            dbo_Posts_GetByUserTest_TestAction.Conditions.Add(emptyResultSetCondition1);
+            resources.ApplyResources(dbo_Posts_GetByUserTest_TestAction, "dbo_Posts_GetByUserTest_TestAction");
+            // 
+            // dbo_Posts_GetByUserTestData
+            // 
+            this.dbo_Posts_GetByUserTestData.PosttestAction = null;
+            this.dbo_Posts_GetByUserTestData.PretestAction = null;
+            this.dbo_Posts_GetByUserTestData.TestAction = dbo_Posts_GetByUserTest_TestAction;
+            // 
+            // emptyResultSetCondition1
+            // 
+            emptyResultSetCondition1.Enabled = true;
+            emptyResultSetCondition1.Name = "emptyResultSetCondition1";
+            emptyResultSetCondition1.ResultSet = 1;
+        }
+
+        #endregion
+
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        #endregion
+
+        [TestMethod()]
+        public void dbo_Posts_GetByUserTest()
+        {
+            SqlDatabaseTestActions testActions = this.dbo_Posts_GetByUserTestData;
+            // Execute the pre-test script
+            // 
+            System.Diagnostics.Trace.WriteLineIf((testActions.PretestAction != null), "Executing pre-test script...");
+            SqlExecutionResult[] pretestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PretestAction);
+            try
+            {
+                // Execute the test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.TestAction != null), "Executing test script...");
+                SqlExecutionResult[] testResults = TestService.Execute(this.ExecutionContext, this.PrivilegedContext, testActions.TestAction);
+            }
+            finally
+            {
+                // Execute the post-test script
+                // 
+                System.Diagnostics.Trace.WriteLineIf((testActions.PosttestAction != null), "Executing post-test script...");
+                SqlExecutionResult[] posttestResults = TestService.Execute(this.PrivilegedContext, this.PrivilegedContext, testActions.PosttestAction);
+            }
+        }
+        private SqlDatabaseTestActions dbo_Posts_GetByUserTestData;
+    }
+}
