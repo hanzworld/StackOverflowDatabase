a3d24d130036a4f906571140965b7e09d057fe6b
Hannah <hannah.gray@xero.com>
2016-10-23 12:00:43 +1300

    Create a scenario where a user login is only targeted to certain enviornments

---
+++ stackoverflow.stackexchange.com/Script.PostDeployment.sql
@@ -0,0 +1,26 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+
+IF (LOWER('$(Environment)') = 'test')
+BEGIN
+	IF NOT EXISTS(SELECT name FROM sys.server_principals WHERE name = 'AutomatedTestLogin')
+	BEGIN
+		CREATE LOGIN AutomatedTestLogin WITH PASSWORD = 'MyAutomatedPassword'
+	END
+
+	IF NOT EXISTS(SELECT name FROM sys.database_principals WHERE name = 'AutomatedTestUser')
+	BEGIN
+		CREATE USER AutomatedTestUser FOR LOGIN AutomatedTestLogin
+	END
+
+END
