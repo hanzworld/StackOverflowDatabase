43a6b824609ebb5f835520f09a5415da91ff178d
Hannah <hannah.gray@xero.com>
2016-10-23 12:02:29 +1300

    Exploring the TSqlModel and TSqlObjects by printing one table to the console. Shows off structure, metadata and properties

--- AutomatedDeploys/Program.cs
+++ AutomatedDeploys/Program.cs
@@ -34,7 +34,55 @@ namespace AutomatedDeploys
 				service.Deploy(mydacpac, connStringBuilder.InitialCatalog, true, deployOptions);
 
 			}
+			PublishDocumentation(@"C:\Dev\StackExchangeDacFx\stackoverflow.stackexchange.com\bin\Debug\stackoverflow.stackexchange.com.dacpac");
+
 			Console.ReadKey();
+		}
+
+		static void PublishDocumentation(string dacpacPath)
+		{
+			var model = TSqlModel.LoadFromDacpac(dacpacPath, new ModelLoadOptions() { LoadAsScriptBackedModel = false, ModelStorageType = DacSchemaModelStorageType.Memory});
+
+			var table = model.GetObject(Table.TypeClass, new ObjectIdentifier(new[] { "dbo", "User" }), DacQueryScopes.UserDefined);
+
+			//foreach (var table in model.GetObjects(DacQueryScopes.UserDefined, Table.TypeClass))
+			//{
+				Console.WriteLine("Table {0}", table.Name);
+
+
+				foreach (var property in table.ObjectType.Properties)
+				{
+
+					Console.WriteLine(": {0} : {1}", property.Name, table.GetProperty(property));
+				}
+
+				Console.Clear();
+
+				foreach (var metadata in table.ObjectType.Metadata)
+				{
+
+					Console.WriteLine(">>> {0} : {1}", metadata.Name, table.GetMetadata(metadata));
+				}
+
+				Console.Clear();
+
+				foreach (var child in table.GetChildren())
+				{
+					Console.WriteLine("> {0} : {1}", child.Name, child.ObjectType.Name);
+
+					foreach (var property in child.ObjectType.Properties)
+					{
+
+						Console.WriteLine(">> {0} : {1}", property.Name, child.GetProperty(property));
+					}
+
+					foreach (var metadata in child.ObjectType.Metadata)
+					{
+
+						Console.WriteLine(">>> {0} : {1}", metadata.Name, child.GetMetadata<ColumnType>(metadata));
+					}
+				}
+			//}
 		}
 	}
 }
